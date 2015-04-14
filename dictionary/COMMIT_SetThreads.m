c8625d5b95667fd2bde62d98615c95d8b7581a69
Alessandro Daducci <alessandro.daducci@gmail.com>
2015-04-14 09:44:13 -0400

    Fatser way to distribute workload

--- dictionary/COMMIT_SetThreads.m
+++ dictionary/COMMIT_SetThreads.m
@@ -11,60 +11,47 @@ function COMMIT_SetThreads( nTHREADS )
     global DICTIONARY KERNELS THREADS
 
     if ( nargin < 1 || nTHREADS < 1 ), nTHREADS = 1; end
-    CONFIG.OPTIMIZATION = [];
-    CONFIG.OPTIMIZATION.nTHREADS = nTHREADS;
     THREADS = [];
+    THREADS.n = nTHREADS;
 
     ticID = tic;
     fprintf( '\n-> Computing the load for each thread\n   ==================================\n' );
-    fprintf( '\t- number of threads : %d\n', CONFIG.OPTIMIZATION.nTHREADS );
+    fprintf( '\t- number of threads : %d\n', THREADS.n );
 
     % Distribute VOXELS to DIFFERENT THREADS to avoid conflicts in A*x product
     % ========================================================================
     fprintf( '\t- A*x product...  ' );
 
     % intra-cellular segments
-    % -----------------------
-    tmp = (CONFIG.OPTIMIZATION.nTHREADS-1)*ones( DICTIONARY.IC.n , 1, 'uint8' );
-    if CONFIG.OPTIMIZATION.nTHREADS>1
-        [~,~,Uidx] = unique(DICTIONARY.IC.v,'stable');
-        C = accumarray(Uidx,1);
-        tID = 0;
-        tot = 0;
-        iPrev = 1;
-        for i = 1:numel(C)
-            if tot >= floor(DICTIONARY.IC.n/CONFIG.OPTIMIZATION.nTHREADS)
-                tmp( Uidx>=iPrev & Uidx<i ) = tID;
-                tID   = tID+1;
-                if tID==CONFIG.OPTIMIZATION.nTHREADS-1, break, end
-                iPrev = i;
-                tot   = C(i);
-            else
+    if DICTIONARY.IC.n > 0
+        THREADS.IC = zeros( nTHREADS+1, 1, 'uint32' );
+        if nTHREADS>1
+            N = floor(DICTIONARY.IC.n/nTHREADS);
+            C = histc(DICTIONARY.IC.v,[0:max(DICTIONARY.IC.v)]);
+            segPerThread = zeros( nTHREADS+1, 1, 'uint32' );
+            tID = 1;
+            tot = 0;
+            for i = 1:numel(C)
                 tot = tot + C(i);
+                if tot >= N
+                    segPerThread( tID+1 ) = tot;
+                    tID   = tID+1;
+                    tot   = 0;
+                end
             end
+            segPerThread( end ) = tot;
+            THREADS.IC = cumsum(segPerThread);
         end
+        THREADS.IC(end) = DICTIONARY.IC.n;
+    else
+        THREADS.IC = [];
     end
-
-    % store only the pointers of the start of each thread's block
-    THREADS.IC = zeros( CONFIG.OPTIMIZATION.nTHREADS+1, 1, 'uint32' );
-    for i = 0:CONFIG.OPTIMIZATION.nTHREADS-1
-        THREADS.IC(i+1) = find( tmp==i, 1, 'first' ) - 1;
-    end
-    THREADS.IC(end) = DICTIONARY.IC.n;
-    clear Uidx tID tot iPrev C
 
     % extra-cellular segments
-    % -----------------------
     if DICTIONARY.EC.nE > 0
-        tmp = (CONFIG.OPTIMIZATION.nTHREADS-1)*ones( DICTIONARY.EC.nE, 1, 'uint8' );
-        for i = 1:CONFIG.OPTIMIZATION.nTHREADS
-            tmp( DICTIONARY.EC.v >= DICTIONARY.IC.v(THREADS.IC(i)+1) & DICTIONARY.EC.v <= DICTIONARY.IC.v(THREADS.IC(i+1)) ) = i-1;
-        end
-
-        % store only the pointers of the start of each thread's block
-        THREADS.EC = zeros( CONFIG.OPTIMIZATION.nTHREADS+1, 1, 'uint32' );
-        for i = 0:CONFIG.OPTIMIZATION.nTHREADS-1
-            THREADS.EC(i+1) = find( tmp==i, 1, 'first' ) - 1;
+        THREADS.EC = zeros( nTHREADS+1, 1, 'uint32' );
+        for i = 1:nTHREADS
+            THREADS.EC(i) = find( DICTIONARY.EC.v == DICTIONARY.IC.v(THREADS.IC(i)+1), 1, 'first' ) - 1;
         end
         THREADS.EC(end) = DICTIONARY.EC.nE;
     else
@@ -72,19 +59,16 @@ function COMMIT_SetThreads( nTHREADS )
     end
 
     % isotropic segments
-    % ------------------
-    tmp = (CONFIG.OPTIMIZATION.nTHREADS-1)*ones( DICTIONARY.nV, 1, 'uint8' );
-    for i = 1:CONFIG.OPTIMIZATION.nTHREADS
-        tmp( DICTIONARY.ISO.v >= DICTIONARY.IC.v(THREADS.IC(i)+1) & DICTIONARY.ISO.v <= DICTIONARY.IC.v(THREADS.IC(i+1)) ) = i-1;
+    if DICTIONARY.nV > 0
+        THREADS.ISO = zeros( nTHREADS+1, 1, 'uint32' );
+        for i = 1:nTHREADS
+            THREADS.ISO(i) = find( DICTIONARY.ISO.v == DICTIONARY.IC.v(THREADS.IC(i)+1), 1, 'first' ) - 1;
+        end
+        THREADS.ISO(end) = DICTIONARY.nV;
+    else
+        THREADS.ISO = [];
     end
 
-    % store only the pointers of the start of each thread's block
-    THREADS.ISO = zeros( CONFIG.OPTIMIZATION.nTHREADS+1, 1, 'uint32' );
-    for i = 0:CONFIG.OPTIMIZATION.nTHREADS-1
-        THREADS.ISO(i+1) = find( tmp==i, 1, 'first' ) - 1;
-    end
-    THREADS.ISO(end) = DICTIONARY.nV;
-    clear tmp i
     fprintf( '[OK]\n' );
 
 
@@ -95,37 +79,57 @@ function COMMIT_SetThreads( nTHREADS )
 
     % intra-cellular segments
     % -----------------------
-    tmp = (CONFIG.OPTIMIZATION.nTHREADS-1)*ones( DICTIONARY.IC.n , 1, 'uint8' );
-    if CONFIG.OPTIMIZATION.nTHREADS>1
-        [~,~,Uidx] = unique(DICTIONARY.IC.fiber,'stable');
-        C = accumarray(Uidx,1);
+%     THREADS.ICt = (nTHREADS-1)*ones( DICTIONARY.IC.n , 1, 'uint8' );
+%     if nTHREADS>1
+%         [~,~,Uidx] = unique(DICTIONARY.IC.fiber,'stable');
+%         C = accumarray(Uidx,1);
+%         tID = 0;
+%         tot = 0;
+%         iPrev = 1;
+%         for i = 1:numel(C)
+%             if tot >= floor(DICTIONARY.IC.n/nTHREADS)
+%                 THREADS.ICt( Uidx>=iPrev & Uidx<i ) = tID;
+%                 tID   = tID+1;
+%                 if tID==nTHREADS-1, break, end
+%                 iPrev = i;
+%                 tot   = C(i);
+%             else
+%                 tot = tot + C(i);
+%             end
+%         end
+%     end
+
+    THREADS.ICt = (nTHREADS-1)*ones( DICTIONARY.IC.n , 1, 'uint8' );
+    if nTHREADS>1
+        [~,idx] = sort(DICTIONARY.IC.fiber);
+        C = histc(DICTIONARY.IC.fiber,[0:max(DICTIONARY.IC.fiber)]);
         tID = 0;
         tot = 0;
-        iPrev = 1;
+        i1 = 1;
+        i2 = 1;
+        N = floor(DICTIONARY.IC.n/nTHREADS);
         for i = 1:numel(C)
-            if tot >= floor(DICTIONARY.IC.n/CONFIG.OPTIMIZATION.nTHREADS)
-                tmp( Uidx>=iPrev & Uidx<i ) = tID;
-                tID   = tID+1;
-                if tID==CONFIG.OPTIMIZATION.nTHREADS-1, break, end
-                iPrev = i;
-                tot   = C(i);
-            else
-                tot = tot + C(i);
+            i2 = i2 + C(i);
+            tot = tot + C(i);
+            if tot >= N
+                THREADS.ICt(i1:i2) = tID;
+                tID = tID+1;
+                if tID==nTHREADS-1, break, end
+                i1 = i2+1;
+                tot = C(i);
             end
         end
     end
+    THREADS.ICt(idx) = THREADS.ICt;
 
-    % store only the pointers of the start of each thread's block
-    THREADS.ICt = tmp;
-    clear Uidx tID tot iPrev C i tmp
 
     % extra-cellular segments
     % -----------------------
     if DICTIONARY.EC.nE > 0
-        THREADS.ECt = zeros( CONFIG.OPTIMIZATION.nTHREADS+1, 1, 'uint32' );
-        n =    floor(DICTIONARY.EC.nE/CONFIG.OPTIMIZATION.nTHREADS);
-        for i = 1:CONFIG.OPTIMIZATION.nTHREADS-1
-            THREADS.ECt(i+1) = THREADS.ECt(i) + n;
+        THREADS.ECt = zeros( nTHREADS+1, 1, 'uint32' );
+        N = floor(DICTIONARY.EC.nE/nTHREADS);
+        for i = 1:nTHREADS-1
+            THREADS.ECt(i+1) = THREADS.ECt(i) + N;
         end
         THREADS.ECt(end) = DICTIONARY.EC.nE;
     else
@@ -134,12 +138,16 @@ function COMMIT_SetThreads( nTHREADS )
 
     % isotropic segments
     % ------------------
-    THREADS.ISOt = zeros( CONFIG.OPTIMIZATION.nTHREADS+1, 1, 'uint32' );
-    n = floor(DICTIONARY.nV/CONFIG.OPTIMIZATION.nTHREADS);
-    for i = 1:CONFIG.OPTIMIZATION.nTHREADS-1
-        THREADS.ISOt(i+1) = THREADS.ISOt(i) + n;
+    if DICTIONARY.nV > 0
+        THREADS.ISOt = zeros( nTHREADS+1, 1, 'uint32' );
+        N = floor(DICTIONARY.nV/nTHREADS);
+        for i = 1:nTHREADS-1
+            THREADS.ISOt(i+1) = THREADS.ISOt(i) + N;
+        end
+        THREADS.ISOt(end) = DICTIONARY.nV;
+    else
+        THREADS.ISO = [];
     end
-    THREADS.ISOt(end) = DICTIONARY.nV;
     fprintf( '[OK]\n' );
 
     fprintf( '   [ %.2f seconds ]\n', toc(ticID) );
