da54fc296ea234e22b3e6dff59ecf662f5a0d8f2
ErickHernandezGutierrez <erick.hernandez@cimat.mx>
2020-03-24 20:33:23 -0600

    Adding kernels for operation A'y in GPU

--- commit/cudaoperator.pyx
+++ commit/cudaoperator.pyx
@@ -134,6 +134,7 @@ cdef class CudaLinearOperator :
         cdef unsigned int  [::1] ISOthreadsT = THREADS['ISOt']
         self.ISOthreadsT = &ISOthreadsT[0]
 
+        """
         idx = np.lexsort( [np.array(self.DICTIONARY['IC']['o']), np.array(self.DICTIONARY['IC']['v'])] )
         self.DICTIONARY['IC']['v']     = self.DICTIONARY['IC']['v'][ idx ]
         self.DICTIONARY['IC']['o']     = self.DICTIONARY['IC']['o'][ idx ]
@@ -145,6 +146,7 @@ cdef class CudaLinearOperator :
         self.DICTIONARY['EC']['v'] = self.DICTIONARY['EC']['v'][ idx ]
         self.DICTIONARY['EC']['o'] = self.DICTIONARY['EC']['o'][ idx ]
         del idx
+        """
 
         self.A = new C_CudaLinearOperator(
             &ICv[0],
