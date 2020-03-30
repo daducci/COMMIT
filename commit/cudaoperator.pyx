65a101ea99661c9da58ea28061e2251150d0a53d
ErickHernandezGutierrez <erick.hernandez@cimat.mx>
2020-03-30 14:33:58 -0600

    Adding memory size and CUDA version checker

--- commit/cudaoperator.pyx
+++ commit/cudaoperator.pyx
@@ -100,7 +100,7 @@ cdef class CudaLinearOperator :
 
         
         self.gpumem = 14*self.n + 6*self.nE + 16*self.nV + 4*(self.nR*self.ndirs*self.nS + self.nT*self.ndirs*self.nS + self.nI*self.nS + self.n1 + self.n2)
-        print('Required GPU Memory = %f GB' % (self.gpumem*1E-6))
+        print('Required GPU Memory = %f GB' % (self.gpumem*1E-9))
         if self.gpumen > 8.0:
             raise RuntimeError( 'GPU Memory exceeded!!!!!!' )
 
