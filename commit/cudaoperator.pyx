35c7fab95c9683c73e4cd1ccd8f7535a797b5b8a
ErickHernandezGutierrez <erick.hernandez@cimat.mx>
2020-04-01 18:49:40 -0600

    Adding fcall flag to CudaLinearOperator

--- commit/cudaoperator.pyx
+++ commit/cudaoperator.pyx
@@ -145,6 +145,7 @@ cdef class CudaLinearOperator :
                 self.nT,
                 self.nI)
 
+            """
             idx = np.lexsort( [np.array(self.DICTIONARY['IC']['o']), np.array(self.DICTIONARY['IC']['fiber'])] )
 
             self.DICTIONARY['IC']['v']     = self.DICTIONARY['IC']['v'][ idx ]
@@ -163,6 +164,7 @@ cdef class CudaLinearOperator :
             self.ICo = &ICo[0]
 
             self.A.setTransposeData(&self.ICv[0], &self.ICf[0], &self.ICo[0], &self.ICl[0])
+            """
 
     @property
     def T( self ) :
