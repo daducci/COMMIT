48bd3bd7c01ebfbe09404e6c800bfae9e2a119bb
frheault <francois.m.rheault@usherbrooke>
2020-05-25 14:15:33 -0400

    Switch to custombuild, python-spams and testing

--- setup.py
+++ setup.py
@@ -1,20 +1,48 @@
-from setuptools import setup
-from setuptools.extension import Extension
-import sys
+from setuptools import Extension, setup
+from setuptools.command.build_ext import build_ext
 
-try:
-    from Cython.Build import cythonize
-except ImportError:
-    def cythonize(*args, **kwargs):
-        from Cython.Build import cythonize
-        return cythonize(*args, **kwargs)
 
-try:
-    from numpy import get_include
-except ImportError:
-    def get_include():
+def get_extensions():
+    # Cython extension to create the sparse data structure from a tractogram
+    # for the computation of matrix-vector multiplications
+    ext1 = Extension(name='commit.trk2dictionary',
+                     sources=['commit/trk2dictionary/trk2dictionary.pyx'],
+                     extra_compile_args=['-w'],
+                     extra_link_args=[],
+                     language='c++')
+
+    ext2 = Extension(name='commit.core',
+                     sources=['commit/core.pyx'],
+                     extra_compile_args=['-w'],
+                     extra_link_args=[],
+                     language='c++')
+
+    ext3 = Extension(name='commit.proximals',
+                     sources=['commit/proximals.pyx'],
+                     extra_compile_args=['-w'],
+                     extra_link_args=[],
+                     language='c++')
+
+    return [ext1, ext2, ext3]
+
+
+class CustomBuildExtCommand(build_ext):
+    """ build_ext command to use when numpy headers are needed. """
+
+    def run(self):
+        # Now that the requirements are installed, get everything from numpy
         from numpy import get_include
-        return get_include()
+        from Cython.Build import cythonize
+
+        # Add everything requires for build
+        self.swig_opts = None
+        self.include_dirs = [get_include()]
+        self.extensions = cythonize(self.extensions)
+
+        # Call original build_ext command
+        build_ext.finalize_options(self)
+        build_ext.run(self)
+
 
 opts = dict(name='dmri-commit',
             version='1.3.7',
@@ -24,40 +52,11 @@ opts = dict(name='dmri-commit',
             author_email='alessandro.daducci@univr.it',
             url='https://github.com/daducci/COMMIT',
             packages=['commit', 'commit.operator'],
+            cmdclass={'build_ext': CustomBuildExtCommand},
+            ext_modules=get_extensions(),
             setup_requires=['Cython>=0.29.17', 'numpy>=1.18.4'],
-            install_requires=['dmri-amico>=1.2.2', 'dipy>=1.1.1'],
+            install_requires=['dmri-amico>=1.2.2', 'dipy>=1.1.1', 'Cython>=0.29.17', 'numpy>=1.18.4'],
             package_data={'commit.operator': ["*.*"]})
 
+setup(**opts)
 
-def get_extensions():
-    # Cython extension to create the sparse data structure from a tractogram
-    # for the computation of matrix-vector multiplications
-    ext1 = Extension(name='commit.trk2dictionary',
-                     sources=['commit/trk2dictionary/trk2dictionary.pyx'],
-                     include_dirs=[get_include()],
-                     extra_compile_args=['-w'],
-                     extra_link_args=[],
-                     language='c++')
-
-    ext2 = Extension(name='commit.core',
-                     sources=['commit/core.pyx'],
-                     include_dirs=[get_include()],
-                     extra_compile_args=['-w'],
-                     extra_link_args=[],
-                     language='c++')
-
-    ext3 = Extension(name='commit.proximals',
-                     sources=['commit/proximals.pyx'],
-                     include_dirs=[get_include()],
-                     extra_compile_args=['-w'],
-                     extra_link_args=[],
-                     language='c++')
-
-    return [ext1, ext2, ext3]
-
-
-if __name__ == '__main__':
-    setup(**opts)
-    if sys.argv[1] != 'clean':
-        opts['ext_modules'] = cythonize(get_extensions())
-        setup(**opts)
