a8900784a9613af293e2538179ff0bff0d2e90bf
frheault <francois.m.rheault@usherbrooke.ca>
2020-06-15 17:34:16 -0400

    Can now build locally

--- commit/core.pyx
+++ commit/core.pyx
@@ -8,7 +8,7 @@ cimport numpy as np
 import time
 import glob
 import sys
-from os import makedirs, remove
+from os import makedirs, remove, getcwd
 from os.path import exists, join as pjoin, isfile
 import nibabel
 import pickle
@@ -17,7 +17,7 @@ import commit.solvers
 import amico.scheme
 import amico.lut
 import pyximport
-pyximport.install( reload_support=True, language_level=3 )
+
 from amico.util import LOG, NOTE, WARNING, ERROR
 
 
@@ -629,7 +629,7 @@ cdef class Evaluation :
         LOG( '   [ %.1f seconds ]' % ( time.time() - tic ) )
 
 
-    def build_operator( self ) :
+    def build_operator( self, build_locally=False, build_dir=None ) :
         """Compile/build the operator for computing the matrix-vector multiplications by A and A'
         using the informations from self.DICTIONARY, self.KERNELS and self.THREADS.
         NB: needs to call this function to update pointers to data structures in case
@@ -657,6 +657,14 @@ cdef class Evaluation :
         config.nIC      = self.KERNELS['wmr'].shape[0]
         config.nEC      = self.KERNELS['wmh'].shape[0]
         config.nISO     = self.KERNELS['iso'].shape[0]
+
+        if build_locally:
+            if build_dir is None:
+                build_dir = getcwd()
+            pyximport.install( reload_support=True, language_level=3, build_in_temp=True, build_dir=build_dir, inplace=False )
+        else:
+            pyximport.install( reload_support=True, language_level=3 )
+
         if not 'commit.operator.operator' in sys.modules :
             import commit.operator.operator
         else :
