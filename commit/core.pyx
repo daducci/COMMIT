c34128cd4750447a9dfdc7dc3548222a259aea98
ErickHernandezGutierrez <erick.hernandez@cimat.mx>
2021-01-13 21:44:39 -0600

    Create cudaoperator folder

--- commit/core.pyx
+++ commit/core.pyx
@@ -716,7 +716,7 @@ cdef class Evaluation :
             self.A = sys.modules['commit.operator.operator'].LinearOperator( self.DICTIONARY, self.KERNELS, self.THREADS )        
         else:
             import commit.cudaoperator
-            self.A = commit.cudaoperator.CudaLinearOperator( self.DICTIONARY, self.KERNELS, self.THREADS, fcall=1 )
+            self.A = commit.cudaoperator.operator.CudaLinearOperator( self.DICTIONARY, self.KERNELS, self.THREADS, fcall=1 )
 
         LOG( '   [ %.1f seconds ]' % ( time.time() - tic ) )
 
