243cad8980f94a452d52cc4bb21eff6cb5d2f097
Alessandro Daducci <alessandro.daducci@univr.it>
2023-02-18 16:11:29 +0100

    Minor mods

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
 
+# name of the package
+package_name = 'commit'
+
 def get_extensions():
     # Cython extension to create the sparse data structure from a tractogram
     # for the computation of matrix-vector multiplications
-    trk2dictionary = Extension(name='commit.trk2dictionary',
-                     sources=['commit/trk2dictionary/trk2dictionary.pyx'],
+    trk2dictionary = Extension(name=f'{package_name}.trk2dictionary',
+                     sources=[f'{package_name}/trk2dictionary/trk2dictionary.pyx'],
                      extra_compile_args=['-w'],
                      language='c++')
-    core = Extension(name='commit.core',
-                     sources=['commit/core.pyx'],
+    core = Extension(name=f'{package_name}.core',
+                     sources=[f'{package_name}/core.pyx'],
                      extra_compile_args=['-w'],
                      language='c++')
-    proximals = Extension(name='commit.proximals',
-                     sources=['commit/proximals.pyx'],
+    proximals = Extension(name=f'{package_name}.proximals',
+                     sources=[f'{package_name}/proximals.pyx'],
                      extra_compile_args=['-w'],
                      language='c++')
     return [trk2dictionary, core, proximals]
@@ -40,9 +43,9 @@ class CustomBuildExtCommand(build_ext):
         build_ext.finalize_options(self)
         build_ext.run(self)
 
-# import details from commit/info.py
+# import details from {package_name}/info.py
 import sys
-sys.path.insert(0, './commit/')
+sys.path.insert(0, f'./{package_name}/')
 import info
 
 # install the package
@@ -55,10 +58,10 @@ setup(
     author_email=info.AUTHOR_EMAIL,
     url=info.URL,
     license=info.LICENSE,
-    packages=['commit', 'commit.operator'],
+    packages=[f'{package_name}', f'{package_name}.operator'],
     cmdclass={'build_ext': CustomBuildExtCommand},
     ext_modules=get_extensions(),
     setup_requires=['Cython>=0.29', 'numpy>=1.12', 'wheel'],
     install_requires=['setuptools>=46.1', 'Cython>=0.29', 'numpy>=1.12', 'scipy>=1.0', 'dipy>=1.0', 'dmri-amico>=1.3.2'],
-    package_data={'commit.operator': ["*.*"]}
+    package_data={f'{package_name}.operator': ["*.*"]}
 )
