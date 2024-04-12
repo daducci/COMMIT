5f7eb1f7fd8d500e67ec81c9cd93619c1cc19ed5
nightwnvol <clorindo.notte@univr.it>
2024-04-12 11:48:01 +0200

    build: add pyproject.toml

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
+import os
+
 from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
-import os
 
 # name of the package
 package_name = 'commit'
@@ -28,9 +29,10 @@ class CustomBuildExtCommand(build_ext):
     """ build_ext command to use when numpy headers are needed. """
     def run(self):
         # Now that the requirements are installed, get everything from numpy
+        from multiprocessing import cpu_count
+
         from Cython.Build import cythonize
         from numpy import get_include
-        from multiprocessing import cpu_count
 
         # Add everything requires for build
         self.swig_opts = None
@@ -46,29 +48,12 @@ class CustomBuildExtCommand(build_ext):
         build_ext.finalize_options(self)
         build_ext.run(self)
 
-# import details from {package_name}/info.py
-import sys
-sys.path.insert(0, f'./{package_name}/')
-import info
-
 # create the 'build' directory
 if not os.path.exists('build'):
     os.makedirs('build')
 
 # install the package
 setup(
-    name=info.NAME,
-    version=info.VERSION,
-    description=info.DESCRIPTION,
-    long_description=info.LONG_DESCRIPTION,
-    author=info.AUTHOR,
-    author_email=info.AUTHOR_EMAIL,
-    url=info.URL,
-    license=info.LICENSE,
-    packages=[f'{package_name}', f'{package_name}.operator'],
     cmdclass={'build_ext': CustomBuildExtCommand},
-    ext_modules=get_extensions(),
-    setup_requires=['Cython>=0.29', 'numpy>=1.12', 'wheel'],
-    install_requires=['setuptools>=46.1', 'Cython>=0.29', 'numpy>=1.12', 'scipy>=1.0', 'dipy>=1.0', 'dmri-dicelib>=1.1.0', 'dmri-amico>=2.0.1'],
-    package_data={f'{package_name}.operator': ["*.*"]}
+    ext_modules=get_extensions()
 )
